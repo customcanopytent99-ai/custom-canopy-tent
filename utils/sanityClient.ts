4025d94392fff078c6bb753522f7811e213acde0
boardape <boardape@uixlabs.in>
2026-04-19 13:11:57 +0530

    ADD: build fix

--- utils/sanityClient.ts
+++ utils/sanityClient.ts
@@ -1,16 +1,15 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
   projectId: "ee0kq0ez",
   dataset: "production",
   apiVersion: "2024-04-17",
-  useCdn: false, // Set to false if you want to ensure fresh data always
+  useCdn: false,
 });
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: SanityImageSource) {
+export function urlFor(source: any) {
   return builder.image(source);
 }
